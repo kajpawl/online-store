6b3faa84d8e5e36fdb3f000d492b410325f163aa
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-08-03 19:12:56 +0200

    Minor visuals fixes

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -8,7 +8,7 @@ const NoMatch = props => {
       <div className="pageWrapper">
         <h2><b>Oh no!</b><span> - we couldn't find your page</span></h2>
         <div className="sadnessWrapper">
-          <img src={require("../../images/sorry.png")} alt="Page not found image" />
+          <img className="meepleLogo" src={require("../../images/sorry.png")} alt="Page not found image" />
           <img className="textLogo" src={require("../../images/logo-text.png")} alt="Page not found - brand logo" />
         </div>
       </div>
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,15 +3,15 @@ import ProductListItem from '../ProductListItem/ProductListItem';
 import './ProductList.scss';
 
 const ProductList = props => (
-	<main className="productList">
-      	<div className="row fixRowPadding">
-					{props.products.map((product, i) => {
-						return (
-							<ProductListItem updateProducts={props.updateProducts} key={i} product={product} addToCart={props.addToCart} cartItems={props.cartItems} />
-						)
-					})}
-				</div>
-	</main>
+  <main className="productList">
+    <div className="row fixRowPadding">
+      {props.products.map((product, i) => {
+        return (
+          <ProductListItem updateProducts={props.updateProducts} key={i} product={product} addToCart={props.addToCart} cartItems={props.cartItems} />
+        )
+      })}
+    </div>
+  </main>
 );
 
 export default ProductList;
